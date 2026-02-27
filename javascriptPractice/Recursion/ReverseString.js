f1047ba4267f9828333852c33b40c7d3d5292cfc
prabhakarzha <kumarprabhakar141@gmail.com>
2026-02-27 09:35:21 +0530

    updated reverseString code

--- javascriptPractice/Recursion/ReverseString.js
+++ javascriptPractice/Recursion/ReverseString.js
@@ -1,17 +1,32 @@
 let str = "Prabhakar";
 let arr = str.split(""); // Convert string to array (strings are immutable in JS)
 
-function reverseString(arr, left, right) {
-  if (left >= right) return;
+function reverseString(str) {
+  let arr = str.split(""); // Convert string to array (strings are immutable in JS)
+  let left = 0;
+  let right = arr.length - 1;
 
-  // swap
-  [arr[left], arr[right]] = [arr[right], arr[left]];
-
-  reverseString(arr, left + 1, right - 1);
+  while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+  return arr.join("");
 }
 
-reverseString(arr, 0, arr.length - 1);
+console.log(reverseString(str));
 
-// Convert back to string
-let reversed = arr.join("");
-console.log(reversed); // rakahbarP
+// function reverseString(arr, left, right) {
+//   if (left >= right) return;
+
+//   // swap
+//   [arr[left], arr[right]] = [arr[right], arr[left]];
+
+//   reverseString(arr, left + 1, right - 1);
+// }
+
+// reverseString(arr, 0, arr.length - 1);
+
+// // Convert back to string
+// let reversed = arr.join("");
+// console.log(reversed); // rakahbarP
