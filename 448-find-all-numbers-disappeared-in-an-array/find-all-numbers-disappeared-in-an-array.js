e8c376db015331976ac6de827ca775c2397939fd
prabhakar kumar <70058887+prabhakarzha@users.noreply.github.com>
2026-03-15 22:15:11 +0530

    Time: 38 ms (10.33%) | Memory: 70.2 MB (98.99%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    const n = nums.length;
+    const count = {};
+    const missing = [];
+    
+    // 1. Count frequency of each number
+    for (let i = 0; i < n; i++) {
+        const num = nums[i];
+        count[num] = (count[num] || 0) + 1;
+    }
+    
+    // 2. Check 1 to n - jiska count 0 hai, wo missing
+    for (let i = 1; i <= n; i++) {
+        if (!count[i]) {  // count[i] undefined ya 0 hoga
+            missing.push(i);
+        }
+    }
+    
+    return missing;
+};
